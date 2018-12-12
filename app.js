4f5e01a5f5e2c003a68320aa57b82297424faa53
laishiwen <467158536@qq.com>
2018-12-12 22:40:15 +0800

    依赖安装

--- app.js
+++ app.js
@@ -5,30 +5,30 @@ const routes = require("./routes")
 const models = require("./models")
 const plugins = require("./plugins")
 
-const initDb = async () => {
-  let sequelize = models.sequelize
-  sequelize.sync({
-    force: false
-  }).then(function () {
-    console.log("connection database successed")
-  }).catch(function (err) {
-    console.log("connection failed due to error: %s", err)
-  })
+const initDb = async() => {
+    let sequelize = await models.sequelize;
+    sequelize.sync({
+        force: false
+    }).then(function(res) {
+        console.log("connection database successed")
+    }).catch(function(err) {
+        console.log("connection failed due to error: %s", err)
+    })
 }
 
-const start = async () => {
-  await server.register(plugins)
+const start = async() => {
+    await server.register(plugins)
 
-  await server.views(views)
+    await server.views(views)
 
-  await routes.forEach(route => server.route(route))
+    await routes.forEach(route => server.route(route))
 
-  await server.start(err => {
-    if (err) throw err
-  })
-  console.log(`Server running at: ${server.info.uri}`)
+    await server.start(err => {
+        if (err) throw err
+    })
+    console.log(`Server running at: ${server.info.uri}`)
 }
 
 start().then(res => {
-  initDb()
+    initDb()
 })
--- config/db.conf.js
+++ config/db.conf.js
@@ -1,18 +1,18 @@
 require("env2")('./.env')
 
 const {
-  env
+    env
 } = process
 
 module.exports = {
-  development: {
-    dialect: env.MYSQL_DIALECT,
-    host: env.MYSQL_HOST,
-    database: env.MYSQL_DB_NAME,
-    username: env.MYSQL_USERNAME,
-    password: env.MYSQL_PASSWORD
-  },
-  production: {
+    development: {
+        dialect: env.MYSQL_DIALECT,
+        host: env.MYSQL_HOST,
+        database: env.MYSQL_DB_NAME,
+        username: env.MYSQL_USERNAME,
+        password: env.MYSQL_PASSWORD
+    },
+    production: {
 
-  }
+    }
 }
