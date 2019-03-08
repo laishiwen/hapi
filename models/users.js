ed927434e13f1fc8c168dd1c5e628e114587c7bc
laishiwen <467158536@qq.com>
2019-03-08 19:15:25 +0800

    update

--- models/users.js
+++ models/users.js
@@ -1,19 +1,19 @@
 module.exports = (sequelize, DataTypes) => sequelize.define(
-    'users',
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-      },
-      nick_name: DataTypes.STRING,
-      avatar_url: DataTypes.STRING,
-      gender: DataTypes.INTEGER,
-      open_id: DataTypes.STRING,
-      session_key: DataTypes.STRING,
+    'users', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        username: DataTypes.STRING,
+        password: DataTypes.STRING,
+        nick_name: DataTypes.STRING,
+        avatar_url: DataTypes.STRING,
+        gender: DataTypes.INTEGER,
+        open_id: DataTypes.STRING,
+        session_key: DataTypes.STRING,
+    }, {
+        tableName: 'users',
     },
-    {
-      tableName: 'users',
-    },
-  );
+);
--- routes/users.js
+++ routes/users.js
@@ -2,26 +2,27 @@ const { authToken, cookie_options } = require("../utils");
 
 const GROUP_NAMW = "users";
 
-module.exports = ({ users }, { userInfoDefine }) => [
-  {
+module.exports = ({ users }, { userInfoDefine }) => [{
     method: "POST",
     path: `/${GROUP_NAMW}/signup`,
-    handler: async (request, h) => {
-      const { userInfo } = request.payload;
+    handler: async(request, h) => {
+        const { username, password } = request.payload.user_info;
+        const result = await users.findAll({
+            where: {
+                username
+            }
+        });
 
-      const result = users.findAll({});
-
-      return h.response("hello").state("token", authToken(888), cookie_options);
+        return h.response("hello").state("token", authToken(888), cookie_options);
     },
     config: {
-      tags: ["api", GROUP_NAMW],
-      description: "用户注册",
-      auth: false,
-      validate: {
-        payload: {
-          ...userInfoDefine
+        tags: ["api", GROUP_NAMW],
+        description: "用户注册",
+        auth: false,
+        validate: {
+            payload: {
+                ...userInfoDefine
+            }
         }
-      }
     }
-  }
-];
+}];
